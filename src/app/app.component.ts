ad6a3254b7e2bff683b5f02faa616062c53d7a7b
Jesus Saul Hernandez <jesus.castaneda@metricamovil.com>
2023-06-13 13:02:47 -0600

    Start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [RouterOutlet],
 })
-export class AppComponent {
-  title = 'mediumclone_angular';
-}
+export class AppComponent {}
