46d840bcbcc96c136dfc834fb030f478e5816a3a
Jesus Saul Hernandez <jesus.castaneda@metricamovil.com>
2023-06-14 11:52:36 -0600

    Register component

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { authActions } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { CommonModule } from '@angular/common';
+import { combineLatest } from 'rxjs';
+import { BackendErrorMessages } from 'src/app/shared/components/backendErrorMessages/backendErrorMessages.component';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.components.html',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink,
+    BackendErrorMessages,
+  ],
+})
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+  });
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+    this.store.dispatch(authActions.register({ request }));
+  }
+}
